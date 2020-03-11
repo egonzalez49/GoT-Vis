319e503a03a822523d0a49a9b9729872c3e830dc
egonzalez49 <egonzalez49@gatech.edu>
2020-03-10 22:23:16 -0400

    Improved network graph readability and added  mouse over effects.

--- main.js
+++ main.js
@@ -1,105 +1,181 @@
+/* SET UP GLOBALS, FORCES, ZOOMING */
 let svg = d3.select('#network');
+let container = svg.append('g');
 
 let width = +svg.attr('width');
 let height = +svg.attr('height');
 
-// outdate code, d3 version 3?
-// let force = d3.layout
-//   .force()
-//   .gravity(0.05)
-//   .distance(100)
-//   .charge(-100)
-//   .size([svgWidth, svgHeight]);
+svg.call(
+    d3
+        .zoom()
+        .scaleExtent([0.1, 4])
+        .on('zoom', function() {
+            container.attr('transform', d3.event.transform);
+        })
+);
+
+let color = d3.scaleOrdinal(d3.schemeCategory10);
 
 let simulation = d3
     .forceSimulation()
-    .force(
-        'link',
-        d3.forceLink().id(function(d) {
-            return d.id;
-        })
-    )
-    .force('charge', d3.forceManyBody())
+    .force('charge', d3.forceManyBody().strength(-1000))
     .force('center', d3.forceCenter(width / 2, height / 2));
 
 let nodesData;
 
+/* LOAD IN CSV DATA */
 d3.csv('./data/network/got-s1-edges.csv').then(links => {
     nodesData = {};
 
     links.forEach(link => {
-        link.source = createNode(link.Source);
-        link.target = createNode(link.Target);
+        createNodes(link);
     });
 
+    /* CREATE A D3 LIST OF NODES */
     let nodes = d3.values(nodesData);
 
-    let link = svg
-        .selectAll('.link')
-        .data(links)
-        .enter()
-        .append('g')
-        .append('line')
-        .attr('class', 'link')
-        .attr('stroke-width', d => {
-            return Math.sqrt(d.Weight);
-        });
-
-    let node = svg
-        .selectAll('.node')
-        .data(nodes)
-        .enter()
-        .append('g')
-        .attr('class', 'node');
-
-    let circles = node
-        .append('circle')
-        .attr('r', 5)
-        .call(
-            d3
-                .drag()
-                .on('start', dragstarted)
-                .on('drag', dragged)
-                .on('end', dragended)
-        );
-
-    let labels = node
-        .append('text')
-        .text(d => {
-            return d.name;
-        })
-        .attr('x', 6)
-        .attr('y', 3);
+    console.log(nodes);
+    console.log(links);
 
     simulation.nodes(nodes).on('tick', ticked);
 
-    simulation.force('link').links(links);
+    simulation.force(
+        'link',
+        d3
+            .forceLink(links)
+            .id(d => {
+                return d.id;
+            })
+            .distance(50)
+            .strength(1)
+    );
 
+    let link = container
+        .append('g')
+        .selectAll('line')
+        .data(links)
+        .enter()
+        .append('line')
+        .attr('class', 'link')
+        .style('stroke-width', d => {
+            return Math.sqrt(d.weight / 4);
+        });
+
+    let node = container
+        .append('g')
+        .selectAll('node')
+        .data(nodes)
+        .enter()
+        .append('circle')
+        .attr('class', 'node')
+        .attr('r', 5)
+        .style('fill', d => {
+            return color(d.group);
+        })
+        .call(
+            d3
+                .drag()
+                .on('start', dragStarted)
+                .on('drag', dragged)
+                .on('end', dragEnded)
+        );
+
+    let label = container
+        .append('g')
+        .selectAll('label')
+        .data(nodes)
+        .enter()
+        .append('text')
+        .attr('class', 'label')
+        .text(d => {
+            return d.id;
+        });
+
+    /* HANDLE NODE HOVER */
+    node.on('mouseover', d => {
+        node.transition(500).style('opacity', n => {
+            if (n === d || d.neighbors.includes(n.id)) {
+                return 1;
+            } else {
+                return 0.2;
+            }
+        });
+
+        link.transition(500).style('opacity', l => {
+            if (d === l.source || d === l.target) {
+                return 1;
+            } else {
+                return 0.2;
+            }
+        });
+
+        label.transition(500).style('opacity', l => {
+            if (l === d || d.neighbors.includes(l.id)) {
+                return 1;
+            } else {
+                return 0.2;
+            }
+        });
+    });
+
+    node.on('mouseout', function() {
+        node.transition(500).style('opacity', 1);
+        link.transition(500).style('opacity', 0.2);
+        label.transition(500).style('opacity', 0.2);
+    });
+
+    /* HANDLES GRAPH TICKS TO DETERMINE NODES/LINKS' POSITIONS */
     function ticked() {
-        link.attr('x1', function(d) {
+        link.attr('x1', d => {
             return d.source.x;
         })
-            .attr('y1', function(d) {
+            .attr('y1', d => {
                 return d.source.y;
             })
-            .attr('x2', function(d) {
+            .attr('x2', d => {
                 return d.target.x;
             })
-            .attr('y2', function(d) {
+            .attr('y2', d => {
                 return d.target.y;
             });
 
-        node.attr('transform', function(d) {
-            return 'translate(' + d.x + ',' + d.y + ')';
+        node.attr('cx', d => {
+            return d.x;
+        }).attr('cy', d => {
+            return d.y;
         });
+
+        label
+            .attr('x', function(d) {
+                return d.x + 8;
+            })
+            .attr('y', function(d) {
+                return d.y + 3;
+            });
     }
 });
 
 function createNode(name) {
-    return nodesData[name] || (nodesData[name] = { name: name });
+    return (
+        nodesData[name] ||
+        (nodesData[name] = { id: name, group: getRandomInt(7), neighbors: [] })
+    );
 }
 
-function dragstarted(d) {
+function createNodes(link) {
+    let sourceData = createNode(link.source);
+    sourceData.neighbors.push(link.target);
+    let targetData = createNode(link.target);
+    targetData.neighbors.push(link.source);
+}
+
+/* TEMPORARY GROUPING NUMBER GENERATOR */
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
+/* D3 FORCE NODE DRAGGING */
+function dragStarted(d) {
     if (!d3.event.active) simulation.alphaTarget(0.3).restart();
     d.fx = d.x;
     d.fy = d.y;
@@ -110,7 +186,7 @@ function dragged(d) {
     d.fy = d3.event.y;
 }
 
-function dragended(d) {
+function dragEnded(d) {
     if (!d3.event.active) simulation.alphaTarget(0);
     d.fx = null;
     d.fy = null;
